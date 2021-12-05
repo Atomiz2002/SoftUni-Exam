48d2bd1b28f5a63cd725890c361bbed1e459b819
Atomiz <tytunipex@gmail.com>
2021-12-05 13:46:26 +0200

    Initial commit

---
+++ Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary {
+    class Program {
+        static void Main(string[] args) {
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+
+            string[] entries = Console.ReadLine().Split(" | ");
+            string[] test = Console.ReadLine().Split(" | ");
+            string final = Console.ReadLine();
+
+            for (var i = 0; i < entries.Length; i++) {
+                string name = entries[i].Split(": ")[0];
+                string desc = entries[i].Split(": ")[1];
+
+                if (dictionary.ContainsKey(name))
+                    dictionary[name].Add(desc);
+                else
+                    dictionary.Add(name, new List<string>() {desc});
+            }
+
+            if (final == "Test") {
+                foreach (string question in test)
+                    if (dictionary.ContainsKey(question)) {
+                        Console.WriteLine($"{question}:");
+                        dictionary[question] = dictionary[question].OrderByDescending(desc => desc.Length).ToList();
+                        dictionary[question].ForEach(desc => Console.WriteLine($" -{desc}"));
+                    }
+            } else {
+                List<string> keys = dictionary.Keys.ToList();
+                keys.Sort();
+                Console.WriteLine(string.Join(" ", keys));
+            }
+        }
+    }
+}
---
+++ Hogwarts/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hogwarts {
+    class Program {
+        static void Main(string[] args) {
+            string text = Console.ReadLine();
+            string[] input;
+
+            while ((input = Console.ReadLine().Split())[0] != "Abracadabra") {
+                switch (input[0]) {
+                    case "Abjuration":
+                        Console.WriteLine(text = text.ToUpper());
+                        break;
+
+                    case "Necromancy":
+                        Console.WriteLine(text = text.ToLower());
+                        break;
+
+                    case "Illusion":
+                        int index = Convert.ToInt32(input[1]);
+
+                        if (index > text.Length - 1) {
+                            Console.WriteLine("The spell was too weak.");
+                            break;
+                        }
+
+                        char replace = input[2].ToCharArray()[0];
+                        char[] chars = text.ToCharArray();
+                        chars[index] = replace;
+
+                        text = new string(chars);
+
+                        Console.WriteLine("Done!");
+                        break;
+
+                    case "Divination":
+                        text = text.Replace(input[1], input[2]);
+                        Console.WriteLine(text);
+                        break;
+
+                    case "Alteration":
+                        text = text.Replace(input[1], "");
+                        Console.WriteLine(text);
+                        break;
+
+                    default:
+                        Console.WriteLine("The spell did not work!");
+                        break;
+                }
+            }
+        }
+    }
+}
